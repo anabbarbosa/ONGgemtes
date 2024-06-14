b282d14275b1e3febdf94e083b078ba2f04c3ea4
anabbarbosa <anabbarbosa06@gmail.com>
2024-06-14 16:10:59 -0300

    conexão com o banco de dados

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<!-- Tela de conexão com o banco de dados, favor não modificar --> 
+
+<?php
+
+ $hostname = "localhost";
+ $bancodedados = "ONG";
+ $usuario = "root";
+ $senha = "";
+
+ $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+ if ($mysqli->connect_errno){
+   echo "Falha ao conectar: (" . $mysql->connect_errno . ") " . $mysql->connect_error;
+ }
+  ?>
+
+  
