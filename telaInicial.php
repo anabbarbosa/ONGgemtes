825e87aabde0cd41de290a0ecb3715058065da0b
Isa <belablima13@gmail.com>
2024-06-24 10:27:34 -0300

    Alterações de texto e páginas

--- telaInicial.php
+++ telaInicial.php
@@ -63,7 +63,7 @@
     <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
       <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
         <h1 class="display-4 fw-bold lh-1 text-body-emphasis">Bem-vindo de volta!</h1>
-        <p class="lead">Algum texto muito massa vai ser colocado aqui depois</p>
+        <p class="lead">O que você quer fazer?</p>
         <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
           <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold azulGemtes bordaAzulGemtes">
             <a class="link-body-emphasis text-decoration-none" href="./assistidos.php">Ver assistidos</a>
